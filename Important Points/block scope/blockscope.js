42239c12a44fb8ed2f494d9a6cf795c3d697f3ba
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-02-02 15:36:44 +0530

    Now, I am understanding block scope variable in JavaScript

---
+++ Important Points/block scope/blockscope.js
@@ -0,0 +1,20 @@
+// 1st block
+
+{
+    let a = 5;
+    // for re-define you can write 
+    // 'let a = 10; 
+    // but
+    // if you want to change either use
+
+    a = 17;
+
+    console.log(a);
+}
+
+// 
+
+{
+    let a = 89;
+    console.log(a)
+}
