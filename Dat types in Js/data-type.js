c6e5c7c08bbec5e08e76663a8706c3613d36b2ae
Adarsh Singh Solanki <111622336+ADARSH29112003@users.noreply.github.com>
2026-02-02 23:11:57 +0530

    Add files via upload
    understanding what is data type of JavaScript

---
+++ Dat types in Js/data-type.js
@@ -0,0 +1,19 @@
+// This is the first Primitive data type which is ''Number''
+let age = 24;
+let price = 100.50;
+let totalexpense = -100.50;
+// This is the 2nd Primitive data type which is ''String''
+let Fullname = "Tony stark";
+let Address = "New Delhi";
+let Title = "Project Grind";
+// This is the 3rd Primitive data type which is "Boolean"
+isfollow = true;
+isSubscribe = false;
+// This is the 4th Primitive data type which is "Undefined" (by default = Every value is undefined until the value is given)
+let a;
+let b;
+let c;
+var z;
+// This is the 5th Primitive data type which is "Null"
+// Very intersting Primitive data type, this is the only data type which is also an object
+let j = null;
