d5fdfdaa60bcc711fe40441d7a846d383d0556c0
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-12 00:12:28 +0530

    know about the changes you have made. You can also use this message to describe why you made these changes.

---
+++ Chapter - 2/Assignment Operators/Assignment.js
@@ -0,0 +1,45 @@
+let a = 16;
+let b = 3;
+
+// SO, in these assignment operators, we can assign the value of varibles to other variables using the assignment operator (=), and many more operations can be performed using the assignment operator.
+
+// In above code, 
+// let a = 16; 
+
+// Here, we are assigning the value 16 to variable 'a' using the assignment operator (=).
+
+// let b = 3; 
+
+// Here, we are assigning the value 3 to variable 'b' using the assignment operator (=).
+
+// And there are many more assignment operators like +=, -=, *=, /=, %=, **= etc. which can be used to perform various operations on the variables.
+
+// Example:
+
+let x = 10; // here we used our first assign operator which is "="
+console.log("x = ", x)
+x += 5;
+// here we used our second assign operator which is "+=" and this operator called addition assignment operator
+// The above code is equivalent to x = x + 5;
+console.log("x = ", x); // Output: 15
+
+x -= 6;
+// here we used our third assign operator which is "-=" and this operator called subtraction assignment operator
+// The above code is equivalent to x = x - 6;
+console.log ("x = ", x); // Output: 9
+
+x *= 9;
+// here we used our fourth assign operator which is "*=" and this operator called multiplication assignment operator
+// The above code is equivalent to x = x * 9;
+console.log("x = ", x); // Output: 81
+
+x /= 3;
+// here we used our fifth assign operator which is "/=" and this operator called division assignment operator
+// The above code is equivalent to x = x / 3;
+console.log("x = ", x); // Output: 27  
+
+x %= 4;
+// here we used our sixth assign operator which is "%=" and this operator called modulus assignment operator
+// The above code is equivalent to x = x % 4;
+console.log("x = ", x); // Output: 3        
+
