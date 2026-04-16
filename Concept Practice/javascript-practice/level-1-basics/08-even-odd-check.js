782d937de752745f675008355802662b8f21b51e
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-16 20:46:13 +0530

    done

--- Concept Practice/javascript-practice/level-1-basics/08-even-odd-check.js
+++ Concept Practice/javascript-practice/level-1-basics/08-even-odd-check.js
@@ -10,3 +10,14 @@
 // Even hai ya Odd
 // Reason likho (IMPORTANT)
 
+let checkValue = 42;
+let remainder  = checkValue % 2;
+
+console.log("remainder = ", remainder); //When you divide 42/2 then quoteint remain 21 and remainder while be zero
+
+if (remainder === 0){ // Here we check if the remainder number is zero then we have to print "Even number" and when we checked the remainder is "0", then system prints "Even Number"
+    console.log("Even Number");
+}
+else {
+    console.log("Odd Number");
+}
---
+++ Concept Practice/javascript-practice/level-1-basics/09-complex-increment.js
@@ -0,0 +1,10 @@
+// Find the value of finalValue and final value of counter:
+
+// let counter = 4;
+// let finalValue = counter++ + ++counter + counter++;
+
+let counter = 4;
+let finalValue = counter++ + ++counter + counter++;
+
+console.log("finalValue = ", finalValue);
+console.log("counter = ", counter);
