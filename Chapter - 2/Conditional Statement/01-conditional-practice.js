fff4dd182c1109ec5e85b2c8e3cdc501f7b3fe81
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-17 23:57:48 +0530

    one more pratice question of conditional operators

---
+++ Chapter - 2/Conditional Statement/01-conditional-practice.js
@@ -0,0 +1,11 @@
+// Get user to input a number using prompt ("Enter a Number"). Check if the number is a multiple of 5 or not.
+
+let num = prompt("Enter your name = ");
+console.log(num);
+
+if(num % 5 === 0){
+    console.log(num, "is multiple of 5");
+} else {
+    console.log(num, "is not multiple of 5")
+}
+    
