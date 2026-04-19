514175d85a886a61178d6fb3740bc51fb06cbf93
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-19 21:49:55 +0530

    Created loops and Strings folder To Understand who loops and strings works and what are the types and uses of it while doing some pratices also

---
+++ Chapter 3 - Loops and Strings/loop-tutorial.js
@@ -0,0 +1,13 @@
+//Print 1 to 5
+
+for(let i = 1; i<= 5 ; i++){
+    console.log("Averona");
+}
+
+//Calculate sum of 1 to 5
+Sum = 0;
+for (let i = 1; i<=5 ; i++){
+    Sum = Sum + i;
+}
+console.log("Sum = ", Sum);
+
