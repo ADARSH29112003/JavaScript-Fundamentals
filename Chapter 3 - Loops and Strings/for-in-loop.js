37dde88f4cd647a6952e663ecb65954b8ad7e0be
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-22 23:53:20 +0530

    Loops Practice begins
    aaj waise ek he question kara kyuki concepts samjhna mein time lagega merko

--- Chapter 3 - Loops and Strings/for-in-loop.js
+++ Chapter 3 - Loops and Strings/for-in-loop.js
@@ -1,3 +1,32 @@
 // for...in kya hota hai?
 
 // Object ke keys (properties) ko loop karne ke liye use hota hai means jo objects ke andar jo keys hai unko ek-ek krke access krna
+
+// ------------Syntax---------------
+
+// for (let key in object){
+//     //kaam
+// }
+
+let person = {
+    name: "Adarsh", // key is - Name
+    age: 22, // key is - Age
+    city: "Varanasi" // key is city
+};
+
+for (let key in person){
+     //person = objects
+    //  console.log(key);
+    //  console.log(person[key]);
+
+    //  console.log("key = ", person[key]);
+    // console.log(key);
+    console.log("key = ", key, ", Object = ", person[key]);
+
+}
+
+//Output
+
+// key =  name , Object =  Adarsh
+// key =  age , Object =  22
+// key =  city , Object =  Varanasi
---
+++ Concept Practice/Loops-Practice/01-practice.js
@@ -0,0 +1,9 @@
+//1. Print all even numbers from 0 to 100.
+
+let number = 0;
+
+for (number = 0; number<=100; number++){
+    if(number % 2 === 0) //for even number
+    // if(number % 2 !== 0) // for odd number
+    console.log("number = ", number);
+}
