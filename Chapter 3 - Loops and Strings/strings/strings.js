a6af0c050bcc902f26dd19bf5f37e2b4cc6c8904
ADARSH29112003 <solankiadarshsingh29112003@gmail.com>
2026-04-25 23:44:59 +0530

    Strings length propterties ko samjh raha hoon

--- Chapter 3 - Loops and Strings/strings/strings.js
+++ Chapter 3 - Loops and Strings/strings/strings.js
@@ -27,3 +27,21 @@ let str2 = 'Averona'; //7
 // Note - length properties also count space all 
 
 // example - "Adarsh Singh Solanki" - Adarsh gives '6', Singh gives '5', Solanki gives '7', so it gives 6+5+7 = 18 right? No, it also count space between them so in "Adarsh Singh Solanki" have 2 space so, 18 + 2 = 20.
+
+// str.length mein ek or khass baat hoti hai like we can particular print an alphabet of given strings. Nahi saamjh aaya wait let me show you above you see, 
+
+// str2 = 'Averona'
+
+// So agar humko "Averona" ka sirf "A" print krwana hai toh kaisa karenge, so in programming basics in memory things stored from zero "0" to depends ki uska andar kya store ho raha hai, so Averona length is 7 right but n back side of storing this in memory is stores like 0,1,2,3,4,5,6 only where, 
+
+// 0 - A
+// 1 - v 
+// 2 - e
+// 3 - r
+// 4 - o
+// 5 - n
+// 6 - a 
+
+// so we write this,
+
+console.log(str2[0]); //A
